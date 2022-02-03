a76cead73668f9556a1da9bb39070166aa643f5e
3rdlab <getthecore@gmail.com>
2022-02-03 16:44:47 +0900

    add collection types

--- src/main/kotlin/week5/4_Types/3_NullableTypes.kt
+++ src/main/kotlin/week5/4_Types/3_NullableTypes.kt
@@ -5,15 +5,28 @@ package week5.`4_Types`
 
     @Nullable Type -> Type?
     @NonNullable Type -> Type
-    Type -> Type! // notation, not syntax. type of unknown nullability
+    Type -> Type! // notation, not syntax. type of unknown nullability -> say it's from Java
 
 
     How to still prevent NPEs?
         - Annotate your Java types
         - Specify types explicitly
 
-
     * Annotate your Java types
+
+    - You can specify @NotNull as Default, and
+    annotate only @Nullable types
+
+    Non-null default JSR-305
+    @ParametersAreNonnullByDefault
+    @MyNonnullByDefault
+
+    package-info.java
+
+    @MyNonnullByDefault
+    package mypackage;
+
+
 
  */
 
---
+++ src/main/kotlin/week5/4_Types/4_CollectionTypes.kt
@@ -0,0 +1,10 @@
+package week5.`4_Types`
+
+/*
+    Java -> Kotlin
+
+    List<String> -> (Mutable)List<String!>
+ */
+
+class `4_CollectionTypes` {
+}
